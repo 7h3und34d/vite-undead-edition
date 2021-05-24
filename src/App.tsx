3e9392afbf59480cc065ee672aa12f28c0b1dd3e
7h3und34d <boaz.dev.21+github@gmail.com>
2021-05-24 23:44:05 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="p-8 font-sans text-3xl font-bold text-indigo-800 bg-indigo-50">
+        React TypeScript Tailwind Vite
+      </h1>
+      <div className="p-20 font-extrabold text-center text-9xl bg-green-50">⚛️</div>
+    </div>
+  );
+}
+
+export default App;
